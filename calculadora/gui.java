c930cb6f61ace06ba7d3caa0b2facd877bc26365
mosterTeco <reyesmls11@hotmail.com>
2024-04-05 12:52:51 -0600

    calculadora

--- calculadora/gui.java
+++ calculadora/gui.java
@@ -1,6 +1,6 @@
 public class gui{
     public static void main(String[] args){
         calculadora c = new calculadora();
-        System.out.println(c.suma(1,2));
+        System.out.println(c.suma(4,6)); // se llama al metodo
     }
 }
